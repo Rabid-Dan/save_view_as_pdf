7a633959711c9a99ba4e263cf9999b0df6d85594
dan morrison <dan@rabidtech.co.nz>
2018-09-21 15:26:48 +1200

    add build rendered pdf method to saveViewAsPDF model

--- lib/save_view_as_pdf.rb
+++ lib/save_view_as_pdf.rb
@@ -7,4 +7,12 @@ class SaveViewAsPdf
   def self.download(filename)
     send_file(pdf.file_path, filename: filename, type: pdf.mime_type)
   end
+
+  private 
+
+  def build_rendered_pdf(html:)
+    renderer = PdfRenderingService.new(from_html: html)
+    renderer.render
+    renderer.pdf
+  end  
 end
