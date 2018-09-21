e4c52e094069f721bdaef6aa6ef085425202b0e7
dan morrison <dan@rabidtech.co.nz>
2018-09-21 15:01:34 +1200

    convert download to self method

--- lib/save_view_as_pdf.rb
+++ lib/save_view_as_pdf.rb
@@ -4,7 +4,7 @@ class SaveViewAsPdf
     @pdf = build_rendered_pdf(html: render_to_string(:print, formats: [:html]))
   end
 
-  def download(filename)
+  def self.download(filename)
     send_file(pdf.file_path, filename: filename, type: pdf.mime_type)
   end
 end
